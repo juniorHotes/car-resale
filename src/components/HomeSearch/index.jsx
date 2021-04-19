beba8ee97d95fc4565ec6a4558d73a07a3df7571
juniorHotes <juniorhotes53@gmail.com>
2021-04-18 23:13:10 -0300

    Refactoring.

--- src/components/HomeSearch/index.jsx
+++ src/components/HomeSearch/index.jsx
@@ -10,10 +10,29 @@ export default function HomeSearch(props) {
         <div className="wrapper">
             <div className="search-container">
                 <form action="">
-                    <Input inputType="text" name="marca" validate="" placeholder="Marca" />
-                    <Input inputType="text" name="modelo" validate="" placeholder="Modelo"/>
-                    <Input inputType="text" name="preço" validate="" placeholder="Preço"/>
-                    <Input inputType="submit"/>
+                    <Input
+                        inputType="text"
+                        name="marca"
+                        placeholder="Marca"
+                    />
+                    <Input
+                        inputType="text"
+                        name="modelo"
+                        placeholder="Modelo"
+                    />
+                    <Input
+                        inputType="text"
+                        name="preço"
+                        placeholder="Preço"
+                    />
+
+                    <Input
+                        size="200px"
+                        inputType="submit"
+                        bgColor="secundary"
+                        value=""
+                        inputSearch="search"
+                    />
                 </form>
             </div>
 
