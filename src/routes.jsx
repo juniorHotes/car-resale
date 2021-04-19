7ec166c40f584a4df46d6b858eeb0dd4480f967b
juniorHotes <juniorhotes53@gmail.com>
2021-04-18 23:11:55 -0300

    New routes.

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Details from './pages/Details';
+import NewUser from './pages/NewUser';
+import Login from './pages/Login';
+import Announce from './pages/Announce';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Home} />
+            <Route path="/details" component={Details} />
+            <Route path="/newuser" component={NewUser} />
+            <Route path="/login" component={Login} />
+            <Route path="/announce" component={Announce} />
         </BrowserRouter>
     );
 }
