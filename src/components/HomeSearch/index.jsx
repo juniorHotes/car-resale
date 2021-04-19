0d505073777f82a6a2e381540400d02b8a987cb2
juniorHotes <juniorhotes53@gmail.com>
2021-04-19 18:46:10 -0300

    Refatoring.

--- src/components/HomeSearch/index.jsx
+++ src/components/HomeSearch/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Form } from '@unform/web'
+import api from '../../services/api'
+
 import './styles.css'
 
 import Input from '../Form/Input'
@@ -8,32 +11,35 @@ export default function HomeSearch(props) {
 
     return (
         <div className="wrapper">
-            <div className="search-container">
-                <form action="">
-                    <Input
-                        inputType="text"
-                        name="marca"
-                        placeholder="Marca"
-                    />
-                    <Input
-                        inputType="text"
-                        name="modelo"
-                        placeholder="Modelo"
-                    />
-                    <Input
-                        inputType="text"
-                        name="preço"
-                        placeholder="Preço"
-                    />
+            <div className="search-wrapper">
+                <div className="search-container">
+                    <Form>
+                        <Input
+                            type="text"
+                            name="marca"
+                            placeholder="Marca"
+                        />
+                        <Input
+                            type="text"
+                            name="modelo"
+                            placeholder="Modelo"
+                        />
+                        <Input
+                            type="text"
+                            name="preço"
+                            placeholder="Preço"
+                        />
 
-                    <Input
-                        size="200px"
-                        inputType="submit"
-                        bgColor="secundary"
-                        value=""
-                        inputSearch="search"
-                    />
-                </form>
+                        <Input
+                            name="submit"
+                            size="200px"
+                            type="submit"
+                            bgColor="secundary"
+                            value=""
+                            inputSearch="search"
+                        />
+                    </Form>
+                </div>
             </div>
 
         </div>
