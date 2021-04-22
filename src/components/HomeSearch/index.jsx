4fcf19fc7edb962a17fa12051cdc0704a1ecc864
juniorHotes <juniorhotes53@gmail.com>
2021-04-22 00:38:32 -0300

    Update.

--- src/components/HomeSearch/index.jsx
+++ src/components/HomeSearch/index.jsx
@@ -13,6 +13,34 @@ export default function HomeSearch(props) {
         <div className="wrapper">
             <div className="search-wrapper">
                 <div className="search-container">
+                    <div className="basic-search-wrapper">
+                        <Form>
+                            <Input
+                                inputSearch="search-input"
+                                input
+                                type="text"
+                                name="search"
+                                placeholder="Pesquise por marca, modelo ou preço"
+                            />
+
+                            <input
+                                className="btn btn-large"
+                                inputSearch="search-button"
+                                type="submit"
+                                value=""
+                            />
+
+                        </Form>
+
+                    </div>
+                    <div className="advanced-button-wrapper">
+                        <button className="waves-effect waves-teal btn-flat"
+                            onClick={e => e.target.ownerDocument.activeElement.firstElementChild.classList.toggle('drop_down')}
+                        >
+                            Pesquisa Avançada
+                            <i className="material-icons arrow_drop_down">arrow_drop_down</i>
+                        </button>
+                    </div>
                     <Form>
                         <Input
                             inputSearch="search-input"
@@ -34,12 +62,6 @@ export default function HomeSearch(props) {
                             placeholder="Preço"
                         />
 
-                        <input
-                            className="btn btn-large"
-                            inputSearch="search-button"
-                            type="submit"
-                            value=""
-                        />
                     </Form>
                 </div>
             </div>
