45421e1c3d0485199d61e183aa25b063f3610863
juniorHotes <juniorhotes53@gmail.com>
2021-05-10 18:36:48 -0300

    Updated.

--- src/pages/MyAds/index.jsx
+++ src/pages/MyAds/index.jsx
@@ -17,6 +17,10 @@ export default function MyAds(props) {
     const { openModal } = useContext(ModalContext)
 
     const [myAds, setMyAds] = useState([])
+    const [myAdsActive, setMyAdsActive] = useState([])
+    const [myAdsInactive, setMyAdsInactive] = useState([])
+    const [myAdsHighlight, setMyAdsHighlight] = useState([])
+
     const [preload, setPreload] = useState(true)
 
     useEffect(async () => {
@@ -48,6 +52,119 @@ export default function MyAds(props) {
         M.Tabs.init(tabRef.current, { swipeable: true });
     }, [])
 
+    useEffect(() => {
+
+        setTimeout(() => {
+            setMyAdsActive(myAdsIsActive())
+            setMyAdsInactive(myAdsIsInactive())
+            setMyAdsHighlight(myAdsIsHighlight())
+    
+            console.log(["Ativos", myAdsActive])
+            console.log(["Inativos", myAdsInactive])
+            console.log(["Destacados", myAdsHighlight])
+            
+        }, 3000);
+
+    }, [myAds])
+
+    const fakeData = [
+        {
+            "id": "b9eb5176-db15-4e15-b7c9-af34fbfb06ed",
+            "brand": "Chevrolet",
+            "model": "Prisma",
+            "image": null,
+            "km": 68000,
+            "potence": "1.6",
+            "price": 41900.00,
+            "subtitle": "",
+            "title": "Prisma 2015 Manual",
+            "year": 2015,
+            "active": true,
+            "highlight": true,
+            "validThrue": "2021-06-02T00:00:00",
+            "register": "2021-05-05T00:00:00"
+        },
+        {
+            "id": "bf8b53f2-b3ec-4155-b679-e89f7e0b4d3e",
+            "brand": "Chevrolet",
+            "model": "Prisma",
+            "image": null,
+            "km": 70000,
+            "potence": "1.0",
+            "price": 40900.00,
+            "subtitle": "",
+            "title": "Prisma 2015 Manual 3",
+            "year": 2012,
+            "active": true,
+            "highlight": false,
+            "validThrue": "2021-06-04T00:00:00",
+            "register": "2021-05-05T00:00:00"
+        },
+        {
+            "id": "bf8b53f2-b3ec-4155-b679-e89f7e0b4d3e",
+            "brand": "Chevrolet",
+            "model": "Prisma",
+            "image": null,
+            "km": 70000,
+            "potence": "1.0",
+            "price": 40900.00,
+            "subtitle": "",
+            "title": "Prisma 2015 Manual 3",
+            "year": 2012,
+            "active": true,
+            "highlight": false,
+            "validThrue": "2021-06-04T00:00:00",
+            "register": "2021-05-05T00:00:00"
+        },
+        {
+            "id": "3bd5ad25-6a75-4f8e-b7a2-7e4331d0b186",
+            "brand": "Chevrolet",
+            "model": "Prisma",
+            "image": null,
+            "km": 68000,
+            "potence": "1.6",
+            "price": 41900.00,
+            "subtitle": "",
+            "title": "Prisma 2015 Manual",
+            "year": 2015,
+            "active": false,
+            "highlight": false,
+            "validThrue": "2021-06-06T00:00:00",
+            "register": "2021-05-05T00:00:00"
+        },
+        {
+            "id": "3bd5ad25-6a75-4f8e-b7a2-7e4331d0b186",
+            "brand": "Chevrolet",
+            "model": "Prisma",
+            "image": null,
+            "km": 68000,
+            "potence": "1.6",
+            "price": 41900.00,
+            "subtitle": "",
+            "title": "Prisma 2015 Manual",
+            "year": 2015,
+            "active": false,
+            "highlight": false,
+            "validThrue": "2021-06-06T00:00:00",
+            "register": "2021-05-05T00:00:00"
+        }
+
+    ]
+
+    function myAdsIsActive() {
+        return fakeData.filter(item => item.active === true)
+    }
+
+    function myAdsIsInactive() {
+        return fakeData.filter(item => item.active === false)
+    }
+
+    function myAdsIsHighlight() {
+        return fakeData.filter(item => item.highlight === true)
+    }
+
+    const keyGenerator = () => (Math.random()*0xFFFFFF<<0).toString(16)
+
     return (
         <>
             <NavBar />
@@ -62,23 +179,23 @@ export default function MyAds(props) {
                         <div className="col s12">
                             <ul ref={tabRef} className="tabs">
                                 <li className="tab col s3 active">
-                                    <a href="#swipe-1">ATIVOS ({myAds.length})</a>
+                                    <a href="#swipe-1">ATIVOS ({myAdsActive.length})</a>
                                 </li>
                                 <li className="tab col s3">
-                                    <a href="#swipe-2">INATIVOS (0)</a>
+                                    <a href="#swipe-2">INATIVOS ({myAdsInactive.length})</a>
                                 </li>
                                 <li className="tab col s3">
-                                    <a href="#swipe-3">DESTACADOS (0)</a>
+                                    <a href="#swipe-3">DESTACADOS ({myAdsHighlight.length})</a>
                                 </li>
                             </ul>
                         </div>
-                        
+
                         <PreloadCircle preload={preload} />
 
                         <div id="swipe-1" className="col s12">
-                            {myAds.map(ads =>
+                            {myAdsActive.map(ads =>
                                 <CardHorizontal
-                                    key={ads.id}
+                                    key={keyGenerator()}
                                     id={ads.id}
                                     image={ads.image}
                                     title={ads.title}
@@ -90,11 +207,37 @@ export default function MyAds(props) {
                                 />
                             )}
                         </div>
+
                         <div id="swipe-2" className="col s12">
-
+                            {myAdsInactive.map(ads =>
+                                <CardHorizontal
+                                    key={keyGenerator()}
+                                    id={ads.id}
+                                    image={ads.image}
+                                    title={ads.title}
+                                    brand={ads.brand}
+                                    model={ads.model}
+                                    price={ads.price}
+                                    register={ads.register}
+                                    validThrue={ads.validThrue}
+                                />
+                            )}
                         </div>
-                        <div id="swipe-3" className="col s12">
 
+                        <div id="swipe-3" className="col s12">
+                            {myAdsHighlight.map(ads =>
+                                <CardHorizontal
+                                    key={keyGenerator()}
+                                    id={ads.id}
+                                    image={ads.image}
+                                    title={ads.title}
+                                    brand={ads.brand}
+                                    model={ads.model}
+                                    price={ads.price}
+                                    register={ads.register}
+                                    validThrue={ads.validThrue}
+                                />
+                            )}
                         </div>
                     </div>
 
